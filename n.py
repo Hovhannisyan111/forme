84bd3b2b8f9788143ddc590f4aab10fc1b436399
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-03-02 18:04:32 +0400

    Guess word

---
+++ n.py
@@ -0,0 +1,12 @@
+print("What is the capital of france: ")
+capital = "paris"
+ms = "-" * len(capital)
+
+while "-" in ms:
+    print(ms)
+    letter = input("Enter a letter: ")
+    
+    for i in range (len(capital)):
+        if capital[i] == letter:
+            ms = ms[:i] + letter + ms[i + 1:]
+print("You win: " , capital)
