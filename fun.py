08e8d8f0c296e171ccf5c12619f803d43d15805b
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-03-17 22:33:02 +0400

    ?

---
+++ fun.py
@@ -0,0 +1,36 @@
+#def unique(ml):
+#    nl = list(set(ml))
+#    return nl
+#ml = [1,2,3,3,3,3,3,4,5]
+#print(unique(ml))
+
+#1
+#def find(mstr,ch):
+#    indexes = []
+#    for i in  range(len(mstr)):
+#        if mstr[i] == ch:
+#            indexes.append(i)
+#    return indexes
+#mstr = "Hello world apeh"
+#ch = 'l'
+#print(find(mstr,ch))
+
+#selection sort
+#def find_smallest(arr):
+#    smallest = arr[0]
+#    smallest_index = 0
+#    for i in range(1, len(arr)):
+#        if arr[i] < smllest:
+#            smallest == arr[i]
+#            smallest_index = i
+#    return smallest_index
+#
+#def selection(arr):
+#    new_arr = []
+#    for i in range(len(arr)):
+#        smallest = find_smallest(arr)
+#        new_arr.append(arr.pop(smallest))
+#    return new_arr
+#arr = [15,7,2,3,0,1,6,8,4,5]
+#print(selection(arr))
+
--- n.py
+++ n.py
@@ -1,13 +1,76 @@
-#Guess the word
-print("What is the capital of france: ")
-capital = "paris"
-ms = "-" * len(capital)
+##Guess the word
+#print("What is the capital of france: ")
+#capital = "paris"
+#ms = "-" * len(capital)
+#
+#while "-" in ms:
+#    print(ms)
+#    letter = input("Enter a letter: ")
+#    
+#    for i in range (len(capital)):
+#        if capital[i] == letter:
+#            ms = ms[:i] + letter + ms[i + 1:]
+#print("You win: " , capital)
 
-while "-" in ms:
-    print(ms)
-    letter = input("Enter a letter: ")
-    
-    for i in range (len(capital)):
-        if capital[i] == letter:
-            ms = ms[:i] + letter + ms[i + 1:]
-print("You win: " , capital)
+def get_content(filename):
+	f = open(filename)
+	return f.read()
+
+def create_user_dict(ml):
+	md = {}
+	for line in ml:
+		line = line.strip().split(": ")
+		md[line[0]] = line[1]	
+	return md
+
+
+def another_read(fname):
+	with open(fname) as f:
+		cnt = f.readlines()
+	return cnt
+
+
+def get_letters_count(mstr):
+	md = {}
+	for el in mstr:
+		if el.isalpha():
+			if el in md:
+				md[el] += 1
+			else:
+				md[el] = 1
+	return md
+
+def word_count(mstr):
+	ml = mstr.split()
+	return len([el for el in ml if el.isalpha()])
+
+def sentence_count(mstr):
+	return len(mstr.split("."))
+
+def sort_dict(md):
+	ml = list(md.items())
+	ml.sort(key=lambda x: x[1])
+	return ml
+
+def custom_write(f, mstr, data):
+	f.write(mstr)
+	f.write(data + "\n")
+	f.write("___\n")
+
+def write_into_file(fname):
+	cnt = get_content("a.txt")
+	letter = get_letters_count(cnt)
+	sorted_list = sort_dict(letter)
+	print(sorted_list)
+	with open(fname, "w") as f:
+		f.write("LETTERS COUNT:\n")
+		f.write("___\n")
+		for k, v in sorted_list:
+			f.write(k + " " + str(v) + "\n")
+		custom_write(f, "WORDS COUNT: ", str(word_count(cnt)))
+		custom_write(f, "SENTENCE COUNT: ", str(sentence_count(cnt)))
+
+		
+write_into_file("results.txt")
+
+
